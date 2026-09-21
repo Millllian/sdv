6ed6d411ce8007c2ba60ac14707175723cebfae1
Maximillian Conrathe (Student) <maximillian.conrathe.2023@uni.strath.ac.uk>
2026-09-21 18:27:14 +0100

    added rain logic

--- soil.py
+++ soil.py
@@ -63,8 +63,11 @@ class soilLayer:
                 if 'F' in self.grid[y][x]:
                     self.grid[y][x].append('X')
                     self.create_soil_tiles()
+                    if self.raining:
+                        self.water_all()
 
     def water(self, target_pos):
+
         for soil_sprite in self.soil_sprites.sprites():
             if soil_sprite.rect.collidepoint(target_pos):
 
@@ -77,6 +80,15 @@ class soilLayer:
 
                 WaterTile(pos, surf, groups=(self.all_sprites, self.water_sprites))
 
+    def water_all(self):
+        for index_row, row in enumerate(self.grid):
+                    for index_col,cell in enumerate(row):
+                        if 'X' in cell and 'W' not in cell:
+                            cell.append('W')
+                            x = index_col * TILE_SIZE
+                            y = index_row * TILE_SIZE
+                            WaterTile((x,y), choice(self.water_surfs), groups=(self.all_sprites, self.water_sprites))
+                            
     def remove_water(self):
 
         #destroy all water sprites
